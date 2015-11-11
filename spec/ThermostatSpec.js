71bcf1f44485c6d03b109b754e29670dadba265c
Deon Tan <deonjtan@gmail.com>
2015-11-11 11:34:12 +0000

    Increase and Decrease temperature functionality implemented

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,5 +1,9 @@
 describe("Thermostat", function(){
-  var thermostat = new Thermostat();
+  var thermostat;
+
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
 
   it("It can read the thermostat temperature", function(){
     expect(thermostat.temperature).toEqual(20);
@@ -7,6 +11,16 @@ describe("Thermostat", function(){
 
   it("Default temeprature is 20", function(){
     expect(thermostat.DEFAULT_TEMPERATURE).toEqual(20);
+  });
+
+  it("Increases temperature with an up button", function(){
+    thermostat.increaseTemperature();
+    expect(thermostat.temperature).toEqual(21);
+  });
+
+  it("Decreases tempearature with a down button", function(){
+    thermostat.decreaseTemperature();
+    expect(thermostat.temperature).toEqual(19);
   });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,12 @@
 function Thermostat() {
   this.DEFAULT_TEMPERATURE = 20;
   this.temperature = this.DEFAULT_TEMPERATURE;
+};
 
+Thermostat.prototype.increaseTemperature = function(){
+  ++this.temperature;
+};
+
+Thermostat.prototype.decreaseTemperature = function(){
+  --this.temperature;
 };
