a39ad7557e0bc72b11b29d01b1e020d0fc506516
Deon Tan <deonjtan@gmail.com>
2015-11-11 17:06:00 +0000

    JQuery implemented. Buttons working

---
+++ html/Thermostat.js
@@ -0,0 +1,47 @@
+function Thermostat() {
+  this.DEFAULT_TEMPERATURE = 20;
+  this.temperature = this.DEFAULT_TEMPERATURE;
+  this.powerSavingMode = true;
+  this.maximumTemperature = 25;
+  this.minimumTemperature = 10;
+}
+
+Thermostat.prototype.increaseTemperature = function(){
+  if (this.temperature == this.maximumTemperature) {
+    throw "Temperature already at maximum temperature";
+  } else {
+    ++this.temperature;
+  }
+};
+
+Thermostat.prototype.decreaseTemperature = function(){
+  if (this.temperature == this.minimumTemperature) {
+    throw "Temperature already at minimum temperature";
+  } else {
+    --this.temperature;
+  }
+};
+
+Thermostat.prototype.turnOffPowerSavingMode = function(){
+  this.powerSavingMode = false;
+  this.maximumTemperature = 32;
+};
+
+Thermostat.prototype.turnOnPowerSavingMode = function(){
+  this.powerSavingMode = true;
+  this.maximumTemperature = 25;
+};
+
+Thermostat.prototype.resetTemperature = function(){
+  this.temperature = this.DEFAULT_TEMPERATURE;
+};
+
+Thermostat.prototype.color = function(){
+  if (this.temperature < 18) {
+    return "Green";
+  } else if (this.temperature < 25) {
+    return "Yellow";
+  } else {
+    return "Red";
+  }
+};
